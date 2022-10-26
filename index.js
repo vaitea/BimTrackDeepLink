91466624f70f99159750a7ba9f36da4a604cc065
Vaitea Doppia <vaitea.doppia@bimone.com>
2022-10-26 15:24:50 -0400

    add route

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const path = require('path')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 6060
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/.well-known/apple-app-site-association', function(req, res) {
+    res.json([{
+      "applinks": {
+          "apps": [],
+          "details": [{
+              "appID": "TLTF7FJX27.com.bimtrack.bimtrack",
+              "paths": ["/*"]
+              }
+          ]
+      }
+  }  
+  ]);
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
